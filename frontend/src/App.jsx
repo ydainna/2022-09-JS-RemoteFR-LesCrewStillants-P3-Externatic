06c05fc1e2eb671ae15b7009216ffd21dbeee229
Yohan0412 <Yohan0412>
2023-01-04 15:46:45 +0100

    footer_desktop_mobile_V1_fini

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
+import Footer from "@components/Footer";
 import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <p>coucou</p>
+      <Footer />
     </div>
   );
 }
