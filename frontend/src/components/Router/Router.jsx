1b43360479fd4a1ccf4278f6582285e661d598c2
saigyou_ayakashi <minuit1993@live.fr>
2023-01-06 10:24:15 +0100

    continu router, create folder for type route

--- frontend/src/components/Router/Router.jsx
+++ frontend/src/components/Router/Router.jsx
@@ -1,9 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "@pages/Home";
-import Register from "@pages/Register";
-import Login from "@pages/Login";
-import Offers from "@pages/Offers";
+
+import Register from "@pages/Auth/Register";
+import Login from "@pages/Auth/Login";
+
+import OfferList from "@pages/Offers/OfferList";
+import Offer from "@pages/Offers/Offer";
+
+import Company from "@pages/Companies/Company";
 
 export default function Router() {
   return (
@@ -12,7 +17,9 @@ export default function Router() {
         <Route index element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/offers" element={<Offers />} />
+        <Route path="/offers" element={<OfferList />} />
+        <Route path="/offers/:id" element={<Offer />} />
+        <Route path="/companies:id" element={<Company />} />
       </Routes>
     </BrowserRouter>
   );
