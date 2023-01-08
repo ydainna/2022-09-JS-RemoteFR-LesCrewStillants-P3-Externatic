59f94c6a6f5c04600a3bd21b013bef4e60490578
Laynaria <pro.laynaria@outlook.fr>
2023-01-08 18:46:07 +0100

    some minor fixes and some improvements to user profile components

--- frontend/src/components/UserProfile/currentSituation/CurrentSituation.jsx
+++ frontend/src/components/UserProfile/currentSituation/CurrentSituation.jsx
@@ -4,8 +4,14 @@ export default function CurrentSituation() {
   return (
     <section id="currentSituation">
       <h1>Situation Actuelle</h1>
-      <p>Recherche Active</p>
-      <p>Recherche Passive</p>
+      <p>
+        Recherche Active <input type="checkbox" />
+      </p>
+
+      <p>
+        Recherche Passive <input type="checkbox" />
+      </p>
+
       <button type="button">Enregistrer</button>
     </section>
   );
