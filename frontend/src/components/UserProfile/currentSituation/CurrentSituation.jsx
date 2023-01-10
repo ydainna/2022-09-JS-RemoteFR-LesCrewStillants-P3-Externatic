8576ca000d820c52c3a8c8d961dcdcc1ed81f84b
Laynaria <pro.laynaria@outlook.fr>
2023-01-10 10:50:34 +0100

    radio input of currentSituation now linked

--- frontend/src/components/UserProfile/currentSituation/CurrentSituation.jsx
+++ frontend/src/components/UserProfile/currentSituation/CurrentSituation.jsx
@@ -6,11 +6,12 @@ export default function CurrentSituation() {
       <form action="">
         <h1>Situation Actuelle</h1>
         <label>
-          Recherche Active <input type="radio" />
+          Recherche Active <input type="radio" name="search" value="active" />
         </label>
 
         <label>
-          Recherche Passive <input type="radio" checked />
+          Recherche Passive{" "}
+          <input type="radio" name="search" value="passive" checked />
         </label>
 
         <button type="button">Enregistrer</button>
