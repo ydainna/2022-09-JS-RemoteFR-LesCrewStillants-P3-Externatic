95b655bdaf8a4aa15a2a6b555a87c8cfdfbb855a
Laynaria <pro.laynaria@outlook.fr>
2023-01-10 09:54:32 +0100

    nearly finished form of presentation

--- frontend/src/components/UserProfile/presentation/Presentation.jsx
+++ frontend/src/components/UserProfile/presentation/Presentation.jsx
@@ -5,35 +5,37 @@ import "./Presentation.scss";
 export default function Presentation() {
   return (
     <section id="presentation">
-      <h1>Présentation</h1>
-      <div>
-        <img src={avatarTemoin} alt="Avatar Témoin" />
+      <form action="">
+        <h1>Présentation</h1>
+        <div>
+          <img src={avatarTemoin} alt="Avatar Témoin" />
 
-        <button type="button">Mettre à jour la photo</button>
-      </div>
+          <button type="button">Mettre à jour la photo</button>
+        </div>
 
-      <label>
-        Civilité{" "}
-        <select name="civility" id="civility-select">
-          <option value="">--Veuillez choisir une option--</option>
-          <option value="M">M</option>
-          <option value="Mme">Mme</option>
-        </select>
-      </label>
-      <label>
-        Prénom <input type="text" />
-      </label>
-      <label>
-        Nom <input type="text" />
-      </label>
-      <label>
-        Téléphone <input type="tel" />
-      </label>
-      <label>
-        Mail <input type="email" />
-      </label>
+        <label>
+          Civilité{" "}
+          <select name="civility" id="civility-select">
+            <option value="">--Veuillez choisir une option--</option>
+            <option value="M">M</option>
+            <option value="Mme">Mme</option>
+          </select>
+        </label>
+        <label>
+          Prénom <input type="text" />
+        </label>
+        <label>
+          Nom <input type="text" />
+        </label>
+        <label>
+          Téléphone <input type="tel" />
+        </label>
+        <label>
+          Mail <input type="email" />
+        </label>
 
-      <button type="button">Enregistrer</button>
+        <button type="button">Enregistrer</button>
+      </form>
     </section>
   );
 }
