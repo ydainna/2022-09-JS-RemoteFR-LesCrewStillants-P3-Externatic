22931886ec1ff2cc84707e77e646f769ba6b0bd4
Laynaria <pro.laynaria@outlook.fr>
2023-01-10 10:14:40 +0100

    changed most of the components to forms it should be okay for now

--- frontend/src/components/UserProfile/currentSituation/CurrentSituation.jsx
+++ frontend/src/components/UserProfile/currentSituation/CurrentSituation.jsx
@@ -3,16 +3,18 @@ import "./CurrentSituation.scss";
 export default function CurrentSituation() {
   return (
     <section id="currentSituation">
-      <h1>Situation Actuelle</h1>
-      <label>
-        Recherche Active <input type="radio" />
-      </label>
+      <form action="">
+        <h1>Situation Actuelle</h1>
+        <label>
+          Recherche Active <input type="radio" />
+        </label>
 
-      <label>
-        Recherche Passive <input type="radio" checked />
-      </label>
+        <label>
+          Recherche Passive <input type="radio" checked />
+        </label>
 
-      <button type="button">Enregistrer</button>
+        <button type="button">Enregistrer</button>
+      </form>
     </section>
   );
 }
--- frontend/src/components/UserProfile/searchParameters/SearchParameters.jsx
+++ frontend/src/components/UserProfile/searchParameters/SearchParameters.jsx
@@ -3,43 +3,45 @@ import "./SearchParameters.scss";
 export default function SearchParameters() {
   return (
     <section id="searchParameters">
-      <h1>Critères de recherche</h1>
+      <form action="">
+        <h1>Critères de recherche</h1>
 
-      <label>
-        Contrat{" "}
-        <select name="contrat" id="contrat-select">
-          <option value="">--Veuillez choisir une option--</option>
-          <option value="Alternance">Alternance</option>
-          <option value="CDI">CDI</option>
-          <option value="CDD">CDD</option>
-          <option value="Stage">Stage</option>
-        </select>
-      </label>
-      <label>
-        Date de Début <input type="date" />
-      </label>
-      <label>
-        Localisation <input type="text" />
-      </label>
-      <label>
-        Télétravail <input type="text" />
-      </label>
-      <label>
-        Poste <input type="text" />
-      </label>
-      <label>
-        Technologie Principale{" "}
-        <select name="techonology" id="techonology-select">
-          <option value="">--Veuillez choisir une option--</option>
-          <option value="Java">Java</option>
-          <option value="Javascript">Javascript</option>
-          <option value="Php">Php</option>
-          <option value="Python">Python</option>
-          <option value="Ruby">Ruby</option>
-        </select>
-      </label>
+        <label>
+          Contrat{" "}
+          <select name="contrat" id="contrat-select">
+            <option value="">--Veuillez choisir une option--</option>
+            <option value="Alternance">Alternance</option>
+            <option value="CDI">CDI</option>
+            <option value="CDD">CDD</option>
+            <option value="Stage">Stage</option>
+          </select>
+        </label>
+        <label>
+          Date de Début <input type="date" />
+        </label>
+        <label>
+          Localisation <input type="text" />
+        </label>
+        <label>
+          Télétravail <input type="text" />
+        </label>
+        <label>
+          Poste <input type="text" />
+        </label>
+        <label>
+          Technologie Principale{" "}
+          <select name="techonology" id="techonology-select">
+            <option value="">--Veuillez choisir une option--</option>
+            <option value="Java">Java</option>
+            <option value="Javascript">Javascript</option>
+            <option value="Php">Php</option>
+            <option value="Python">Python</option>
+            <option value="Ruby">Ruby</option>
+          </select>
+        </label>
 
-      <button type="button">Enregistrer</button>
+        <button type="button">Enregistrer</button>
+      </form>
     </section>
   );
 }
