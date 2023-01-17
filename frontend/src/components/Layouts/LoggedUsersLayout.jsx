f9900eb01658c01c4683ce184dd1c10e6bbc3ced
Anthony Gorski <[anthony.gorski@wildcodeschool.com](mailto:anthony.gorski@wildcodeschool.com)>
2023-01-17 10:56:45 +0100

    I don't inderstand...

--- frontend/src/components/Layouts/LoggedUsersLayout.jsx
+++ frontend/src/components/Layouts/LoggedUsersLayout.jsx
@@ -1,4 +1,4 @@
-import SubNavUsers from "../UserProfile/subNavUsers/SubNavUsers";
+import SubNavUsers from "@components/UserProfile/subNavUsers/SubNavUsers";
 
 export default function LoggedUsersLayout({ children }) {
   return (
