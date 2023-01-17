55ef84a8deeb9780f86c0c502e1bac0c9560553e
Anthony Gorski <[anthony.gorski@wildcodeschool.com](mailto:anthony.gorski@wildcodeschool.com)>
2023-01-17 11:14:29 +0100

    :sob:

--- frontend/src/components/Layouts/LoggedUsersLayout.jsx
+++ frontend/src/components/Layouts/LoggedUsersLayout.jsx
@@ -1,4 +1,4 @@
-import SubNavUsers from "@components/UserProfile/subNavUsers/SubNavUsers";
+import SubNavUsers from "@components/UserProfile/SubNavUsers/SubNavUsers";
 
 export default function LoggedUsersLayout({ children }) {
   return (
