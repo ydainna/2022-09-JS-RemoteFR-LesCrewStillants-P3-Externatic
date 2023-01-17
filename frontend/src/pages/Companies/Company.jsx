7bbb2adf9fa68d4833b21598951aa17b684a2eb5
LAURE JANIN <ljanin51@gmail.com>
2023-01-17 12:34:57 +0100

    add function map on company + css

--- frontend/src/pages/Companies/Company.jsx
+++ frontend/src/pages/Companies/Company.jsx
@@ -5,6 +5,16 @@ import eye from "@assets/icons/eye.svg";
 import "../../components/Company/company.scss";
 
 export default function Company() {
+  const arrayCompagnies = [
+    {
+      id: "1",
+      name: "Nom de l'entreprise",
+      desc: "Une super entreprise avec tout plein de choses chouettes et des babyfoot.",
+      link: "https://www.externatic.fr/",
+      sector: "Industrie",
+    },
+  ];
+
   const arrayOffersCompany = [
     { id: "1", name: "Nom de l'offre", city: "Lyon" },
     { id: "2", name: "Nom de l'offre", city: "Lille" },
@@ -15,25 +25,25 @@ export default function Company() {
     { id: "7", name: "Nom de l'offre", city: "Paris" },
     { id: "8", name: "Nom de l'offre", city: "Tour" },
   ];
+
   return (
     <>
-      <section className="entreprise">
-        <img src={bannerOffer} alt="Entreprise" />
-        <div className="presentation_entreprise">
-          <div className="name_entreprise">
-            <h2>Nom de l'entreprise</h2>
-            <p>Description de l'entreprise</p>
-            <a
-              href="https://www.externatic.fr/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Lien vers l'entreprise
-            </a>
+      {arrayCompagnies.map((compagny) => (
+        <section key={compagny.id} className="entreprise">
+          <img src={bannerOffer} alt="Entreprise" />
+
+          <div className="presentation_entreprise">
+            <div className="name_entreprise">
+              <h2>{compagny.name}</h2>
+              <p>{compagny.desc}</p>
+              <a href={`${compagny.link}`} target="_blank" rel="noreferrer">
+                Voir le site de l'entreprise
+              </a>
+            </div>
           </div>
-        </div>
-        <h3>Secteur</h3>
-      </section>
+          <h3>{compagny.sector}</h3>
+        </section>
+      ))}
       <section className="offres_entreprise">
         <h2 className="Liste_offres">Les offres de l'entreprise</h2>
         {arrayOffersCompany.map((offer) => (
