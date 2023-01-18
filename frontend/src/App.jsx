f0eaad65fde68d3a9d9808bac5ed03f04cab2a74
Laynaria <pro.laynaria@outlook.fr>
2023-01-18 09:31:57 +0100

    replaced Router to be the only component in App, last commit was a failure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,10 @@
-import Footer from "@components/Footer";
-import ComposantLogin from "@components/ComposantLogin";
 import Router from "@components/Router/Router";
-import Header from "./components/Header/Header";
 
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <ComposantLogin />
-      <Footer />
-
       <Router />
     </div>
   );
