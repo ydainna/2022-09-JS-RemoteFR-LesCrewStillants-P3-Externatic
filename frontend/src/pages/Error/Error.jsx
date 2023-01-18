9a3d9e15719eed1101e1835b2bed57f93ebbf288
saigyou_ayakashi <minuit1993@live.fr>
2023-01-18 09:41:26 +0100

    add style 404

--- frontend/src/pages/Error/Error.jsx
+++ frontend/src/pages/Error/Error.jsx
@@ -1,8 +1,34 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+import sadPage from "../../assets/lottie/sadPage.json";
+
+import "@components/Error/Error.scss";
+
 export default function Error() {
   return (
-    <div>
-      <h1>404</h1>
-      <h2>Page not found</h2>
-    </div>
+    <>
+      <div className="notFoundContainer">
+        <Player autoplay loop src={sadPage} className="notFoundLottie" />
+        <button type="button" className="notFoundButton">
+          Go Back
+        </button>
+      </div>
+      {/*
+        __                 
+        '. \  🅷🅴🅻🅻🅾, 🅼🆈 🅽🅰🅼🅴 🅸🆂 🅴🅻🅼🅴🆁 ❗
+        '- \               
+          / /_         .---.
+        / | \\,.\/--.//    )
+        |  \//        )/  / 
+          \  ' ^ ^    /    )____.----..  6
+          '.____.    .___/            \._) 
+              .\/.                      )
+              '\                       /
+              _/ \/    ).        )    (
+              /#  .!    |        /\    /
+              \  C// #  /'-----''/ #  / 
+          .   'C/ |    |    |   |    |mrf  ,
+          \), .. .'OOO-'. ..'OOO'OOO-'. ..\(,
+      */}
+    </>
   );
 }
