eeda5fa9383545f50db79ea8be17ca7d901407f7
Laynaria <pro.laynaria@outlook.fr>
2023-01-18 09:44:00 +0100

    some of my commits didn't merge, don't know why, adding them now for user management page

--- frontend/src/pages/Admin/UsersManagement.jsx
+++ frontend/src/pages/Admin/UsersManagement.jsx
@@ -6,59 +6,59 @@ export default function UsersManagement() {
   const arrayCandidature = [
     {
       id: 1,
-      name: "Développeur Web Front-End",
-      city: "Paris",
-      feedback: "Retour Entreprise",
-      meeting: "Entretien",
+      lastname: "Dupont",
+      firstname: "Martine",
+      email: "martine.dupont@email.com",
+      role: "Candidat",
     },
     {
       id: 2,
-      name: "Développeur Web Front-End",
-      city: "Lille",
-      feedback: "Retour Entreprise",
-      meeting: "Entretien",
+      lastname: "Elmer",
+      firstname: "Valentin",
+      email: "valentin.elmer@externatic.fr",
+      role: "Consultant",
     },
     {
       id: 3,
-      name: "Développeur Web Front-End",
-      city: "Marseille",
-      feedback: "Retour Entreprise",
-      meeting: "Entretien",
+      lastname: "Durand",
+      firstname: "Patrick",
+      email: "patric.durand@externatic.fr",
+      role: "Admin",
     },
     {
       id: 4,
-      name: "Développeur Web Front-End",
-      city: "Lyon",
-      feedback: "Retour Entreprise",
-      meeting: "Entretien",
+      lastname: "Dugoûter",
+      firstname: "Alicia",
+      email: "aliciadugouter@gmail.fr",
+      role: "Candidat",
     },
     {
       id: 5,
-      name: "Développeur Web Front-End",
-      city: "Londres",
-      feedback: "Retour Entreprise",
-      meeting: "Entretien",
+      lastname: "Castanor",
+      firstname: "Estelle",
+      email: "estelle.castanor@yahoo.org",
+      role: "Candidat",
     },
     {
       id: 6,
-      name: "Développeur Web Front-End",
-      city: "Bordeaux",
-      feedback: "Retour Entreprise",
-      meeting: "Entretien",
+      lastname: "Dupuis",
+      firstname: "Marie",
+      email: "marie.dupuis@msn.fr",
+      role: "Candidat",
     },
     {
       id: 7,
-      name: "Développeur Web Back-End Symfony",
-      city: "Paris",
-      feedback: "Retour Entreprise",
-      meeting: "Entretien",
+      lastname: "Doe",
+      firstname: "John",
+      email: "johndoe@yopmail.com",
+      role: "Candidat",
     },
     {
       id: 8,
-      name: "Développeur Web Front-End",
-      city: "Paris",
-      feedback: "Retour Entreprise",
-      meeting: "Entretien",
+      lastname: "Pivert",
+      firstname: "Georges",
+      email: "georges.pivert@externatic.fr",
+      role: "Consultant",
     },
   ];
   return (
@@ -69,20 +69,34 @@ export default function UsersManagement() {
           <button type="button">Supprimer le profil</button>
           <button type="button">Créer un compte consultant</button>
         </div>
+        <div className="users-management-filter">
+          <input type="search" name="" id="" placeholder="Rechercher..." />
+          <select name="" id="">
+            <option value="Rôles">Rôles</option>
+            <option value="Candidat">Candidats</option>
+            <option value="Consultant">Consultants</option>
+            <option value="Admin">Admins</option>
+          </select>
+          <button type="button">Réinitialiser ma rechercher</button>
+        </div>
         <table width="100%">
           <tbody>
             <tr>
-              <th>Offre</th>
-              <th>Retour Entreprise</th>
-              <th>Entretien</th>
+              <th> </th>
+              <th>Nom</th>
+              <th>Prénom</th>
+              <th>Email</th>
+              <th>Rôles</th>
             </tr>
             {arrayCandidature.map((candidature) => (
               <tr key={candidature.id}>
                 <td>
-                  {candidature.name} - {candidature.city}
+                  <input type="checkbox" name="" id="" />
                 </td>
-                <td>{candidature.feedback}</td>
-                <td>{candidature.meeting}</td>
+                <td>{candidature.lastname}</td>
+                <td>{candidature.firstname}</td>
+                <td>{candidature.email}</td>
+                <td>{candidature.role}</td>
               </tr>
             ))}
           </tbody>
