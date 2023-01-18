07d75a68fe83bc688de8384700c20c0f5b9a9e21
LAURE JANIN <ljanin51@gmail.com>
2023-01-18 17:50:28 +0100

    connect route login back to front

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,20 +1,62 @@
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
 
 import "../../components/Auth/Login/Login.scss";
 
 export default function Login() {
+  const [loginUser, setLoginUser] = useState("");
+  const navigate = useNavigate();
+
+  const instance = axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_URL,
+    withCredentials: false,
+  });
+
+  const handleChangeLogin = (e) => {
+    const { name, value } = e.target;
+    setLoginUser({ ...loginUser, [name]: value });
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    instance
+      .post("/login", loginUser)
+      .then(() => navigate("/profile"))
+      .catch((err) =>
+        console.error(err, toast.error("Wrong informations ! ❌"))
+      );
+  };
+
   return (
     <div className="connexion">
+      <ToastContainer
+        theme="dark"
+        autoClose={2000}
+        position="bottom-center"
+        className="toast-container"
+        toastClassName="dark-toast"
+      />
       <div className="rectangle">
         <p className="texte">Connexion : </p>
-
-        <form>
+        <form onSubmit={handleLogin}>
           <label htmlFor="email">Email : </label>
-          <input className="input-mail" type="email" />
+          <input
+            className="input-mail"
+            type="email"
+            onChange={handleChangeLogin}
+            required
+          />
           <br />
           <br />
           <label htmlFor="mdp">Mot De Passe : </label>
-          <input className="input-mdp" type="password" />
+          <input
+            className="input-mdp"
+            type="password"
+            onChange={handleChangeLogin}
+            required
+          />
           <a href="https://support.google.com/accounts/answer/7682439?hl=fr">
             Mot de passe oublié
           </a>
