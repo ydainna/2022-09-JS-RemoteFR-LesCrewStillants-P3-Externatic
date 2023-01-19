42cd3a2e04f35515bfc6da72dda1df8ae3f5e042
LAURE JANIN <ljanin51@gmail.com>
2023-01-19 14:37:27 +0100

    clean code

--- frontend/src/pages/Users/Profile.jsx
+++ frontend/src/pages/Users/Profile.jsx
@@ -14,15 +14,7 @@ export default function Profile() {
   const token = sessionStorage.getItem("token");
 
   const reloadInfo = () => {
-    // depuis le back, on res.json({ message: "Ok", token});
-    // depuis le front, on récupère le token
-    // option, session ou context
-    // sessionStorage.setItem("token", res.data.token);
-    // const token: sessionStorage.getItem('token')
-    // console.log(jwtDecode(token));
-
     const decodedHeader = jwtDecode(token);
-    console.warn(decodedHeader);
 
     instance.get(`/users/${decodedHeader.id}`).then((response) => {
       setInfo(response.data);
