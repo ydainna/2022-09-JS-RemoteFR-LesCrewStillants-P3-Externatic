0ccdacea3e416e65bc551b7f710e2ff32e039a06
Alicia-Dupil <dupil.alicia@gmail.com>
2023-01-20 11:22:16 +0100

    debute route offer information and company

--- frontend/src/components/Offers/CardOffer.jsx
+++ frontend/src/components/Offers/CardOffer.jsx
@@ -1,20 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import instance from "@utils/instance";
 
 import Heart from "@assets/icons/Heart.svg";
 import "./cardofferstyle.css";
 
-function CardOffer({ offer }) {
+function CardOffer() {
   const [isFavorite, setIsFavorite] = useState(false);
+  const [offers, setOffers] = useState([]);
+  // const [company, setCompany] = useState([]);
+  // const [information, setInformation] = useState([]);
+
+  useEffect(() => {
+    instance
+      .get("/offers")
+      .get("/compagny")
+      .get("/information")
+      .then((result) => {
+        setOffers(result.data);
+        // setCompany(result.data);
+        // setInformation(result.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <div className="card">
       <div className="card-personalize">
         <div className="card-header">
-          <img src={offer.img} alt="company offer description" />
+          {/* <img src={company.banner} alt="company offer description" /> */}
         </div>
         <div className="card-body">
-          <h2 className="card-title">{offer.name}</h2>
-          <p className="card-description">{offer.desc}</p>
-          <p className="card-contract">{offer.contrat}</p>
+          <h2 className="card-title">{offers.title}</h2>
+          <p className="card-description">{offers.job_description}</p>
+          {/* <p className="card-contract">{information.type_of_contract}</p> */}
         </div>
       </div>
       <div className="offerbuttons">
