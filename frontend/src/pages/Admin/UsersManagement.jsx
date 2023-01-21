2ff8e2e694e5976ee09e457331a21bad69ea2024
Laynaria <pro.laynaria@outlook.fr>
2023-01-21 14:05:46 +0100

    role filter now works on users management page

--- frontend/src/pages/Admin/UsersManagement.jsx
+++ frontend/src/pages/Admin/UsersManagement.jsx
@@ -5,8 +5,11 @@ import SpecialUsersLayout from "@components/Layouts/SpecialUsersLayout";
 import "@components/ManagementsPages/Admin/UsersManagement.scss";
 
 export default function UsersManagement() {
-  const [arrayCandidature, setArrayCandidature] = useState([]);
   const arrayRoleName = ["Admin", "Candidat", "Consultant"];
+
+  const [arrayCandidature, setArrayCandidature] = useState([]);
+
+  const [roleFilter, setRoleFilter] = useState(0);
 
   useEffect(() => {
     instance
@@ -29,11 +32,15 @@ export default function UsersManagement() {
         </div>
         <div className="users-management-filter">
           <input type="search" name="" id="" placeholder="Rechercher..." />
-          <select name="" id="">
-            <option value="Rôles">Rôles</option>
-            <option value="Candidat">Candidats</option>
-            <option value="Consultant">Consultants</option>
-            <option value="Admin">Admins</option>
+          <select
+            name=""
+            id=""
+            onChange={(e) => setRoleFilter(parseInt(e.target.value, 10))}
+          >
+            <option value="0">Rôles</option>
+            <option value="2">Candidats</option>
+            <option value="3">Consultants</option>
+            <option value="1">Admins</option>
           </select>
           <button type="button">Réinitialiser ma rechercher</button>
         </div>
@@ -46,17 +53,22 @@ export default function UsersManagement() {
               <th>Email</th>
               <th>Rôles</th>
             </tr>
-            {arrayCandidature.map((candidature) => (
-              <tr key={candidature.id}>
-                <td>
-                  <input type="checkbox" name="" id="" />
-                </td>
-                <td>{candidature.lastname}</td>
-                <td>{candidature.firstname}</td>
-                <td>{candidature.email}</td>
-                <td>{arrayRoleName[candidature.role_id - 1]}</td>
-              </tr>
-            ))}
+            {arrayCandidature
+              .filter(
+                (candidat) =>
+                  candidat.role_id === roleFilter || roleFilter === 0
+              )
+              .map((candidature) => (
+                <tr key={candidature.id}>
+                  <td>
+                    <input type="checkbox" name="" id="" />
+                  </td>
+                  <td>{candidature.lastname}</td>
+                  <td>{candidature.firstname}</td>
+                  <td>{candidature.email}</td>
+                  <td>{arrayRoleName[candidature.role_id - 1]}</td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </section>
