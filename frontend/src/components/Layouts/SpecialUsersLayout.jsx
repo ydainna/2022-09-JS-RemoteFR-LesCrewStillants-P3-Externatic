0e4b9ed608bc54deeb233e3e7a1bb477821859bf
Laynaria <pro.laynaria@outlook.fr>
2023-02-08 12:28:10 +0100

    if not log, consultants pages and admins aren't accessible. Need to find how to make it work by role now

--- frontend/src/components/Layouts/SpecialUsersLayout.jsx
+++ frontend/src/components/Layouts/SpecialUsersLayout.jsx
@@ -1,7 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+import instance from "@utils/instance";
 import SubNavSpecialUsers from "@components/ManagementsPages/subNavUsers/SubNavSpecialUsers";
 
 export default function SpecialUsersLayout() {
+  const [role, setRole] = useState(0);
+  const token = sessionStorage.getItem("token");
+  const navigate = useNavigate();
+
+  const reloadInfo = () => {
+    if (token !== null) {
+      const decodedHeader = jwtDecode(token);
+
+      return instance
+        .get(`/users/${decodedHeader.id}`)
+        .then((response) => {
+          setRole(response.data.role_id);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+    return navigate("/login");
+  };
+
+  useEffect(() => {
+    reloadInfo();
+    console.warn(role);
+  }, []);
+
   return (
     <>
       <SubNavSpecialUsers />
