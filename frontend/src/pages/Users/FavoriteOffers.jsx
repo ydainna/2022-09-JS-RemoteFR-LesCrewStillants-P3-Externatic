c90289ecfd9c0b2913f0aec9f15b91116cb430db
LAURE JANIN <ljanin51@gmail.com>
2023-02-08 14:12:53 +0100

    ajout de lottie lorsqu'aucune offre n'a été mise en favoris ou que le candidat n'a postulé à aucune offre dans les page favoris et candidature

--- frontend/src/pages/Users/FavoriteOffers.jsx
+++ frontend/src/pages/Users/FavoriteOffers.jsx
@@ -1,14 +1,17 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Player } from "@lottiefiles/react-lottie-player";
 import jwtDecode from "jwt-decode";
 import heart from "@assets/icons/Heart.svg";
 import instance from "@utils/instance";
+import empty from "../../assets/lottie/empty.json";
 
 import "@components/UserProfile/FavoritesOffers.scss";
 
 export default function FavoriteOffers() {
   const token = sessionStorage.getItem("token");
   const [arrayFavorite, setArrayFavorite] = useState([]);
+  const [isFavorite, setIsFavorite] = useState(true);
 
   const navigate = useNavigate();
 
@@ -17,6 +20,7 @@ export default function FavoriteOffers() {
       .get(`/user-offers/${decodedHeader.id}`)
       .then((result) => {
         setArrayFavorite(result.data);
+        setIsFavorite(result.data.length === 0);
       })
       .catch((err) => {
         console.error(err);
@@ -32,18 +36,58 @@ export default function FavoriteOffers() {
     return navigate("/login");
   }, []);
 
+  // const handleLike = () => {
+  //   if (isFavorite) {
+  //     instance
+  //       .delete(`/uoffer/${user}/${offer.id}`)
+  //       .then(() => {
+  //         setIsFavorite(false);
+  //       })
+  //       .catch((err) => {
+  //         console.error(err);
+  //       });
+  //   } else {
+  //     instance
+  //       .post(`/uoffer`, {
+  //         isFavorite: true,
+  //         isApplied: false,
+  //         user_id: user,
+  //         offer_id: offer.id,
+  //         consultant_id: company.user_id,
+  //       })
+  //       .then(() => {
+  //         setIsFavorite(true);
+  //       })
+  //       .catch((err) => {
+  //         console.error(err);
+  //       });
+  //   }
+  // };
+
   return (
     <section className="favorite-offers">
-      {arrayFavorite
-        .filter((fav) => fav.isFavorite === 1)
-        .map((favorite) => (
-          <p key={favorite.id}>
-            <span>
-              {favorite.title} - {favorite.localisation}
-            </span>
-            <img src={heart} alt="favorite icon" />
-          </p>
-        ))}
+      {isFavorite ? (
+        <>
+          Vous n'avez pas encore mis d'offres en favoris
+          <br />
+          <br />
+          <br />
+          <Player autoplay loop src={empty} className="notFoundLottie" />
+        </>
+      ) : (
+        <>
+          {arrayFavorite
+            .filter((fav) => fav.isFavorite === 1)
+            .map((favorite) => (
+              <p key={favorite.id}>
+                <span>
+                  {favorite.title} - {favorite.localisation}
+                </span>
+                <img src={heart} alt="favorite icon" />
+              </p>
+            ))}
+        </>
+      )}
     </section>
   );
 }
